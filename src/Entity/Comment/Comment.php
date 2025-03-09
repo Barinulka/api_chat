3ee9c6331a608e05e33bd8af9cf70e5948a5a09a
al.barinov <barinulka@yandex.ru>
2025-03-09 13:58:28 +0300

    feat: add comment entty

---
+++ src/Entity/Comment/Comment.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Entity\Comment;
+
+use App\Entity\Post\Post;
+use App\Entity\User\User;
+
+class Comment
+{
+    public function __construct(
+        public int $id,
+        public User $user,
+        public Post $post,
+        public string $comment,
+    ) {
+
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(Post $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
